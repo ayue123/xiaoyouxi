9c36dddad3cd271e9eab56d58eac04fb0b958da5
ayue123 <1759467285@qq.com>
2020-09-15 16:50:45 +0800

    2020.09.15
    资源整理

--- workspace/blct_client/assets/Script/Core/Cmd/Protocol/Request1001.ts
+++
@@ -1,7 +0,0 @@
-import request from "./Request";
-
-export default class Request1001 implements request {
-    public playerId: number;
-
-}
-
--- workspace/blct_client/assets/Script/Core/Cmd/Protocol/Response1001.ts
+++
@@ -1,7 +0,0 @@
-import response  from "./Response";
-
-export default class Response1001 implements  response  {
-    public  userId:string;
-    public name:string;
-
-}
